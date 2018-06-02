4fe9317b3d986b0c7b6edca2c121293def726ae6
Rahul Bhatia <rbhatia46@gmail.com>
2018-06-02 07:37:14 +0530

    Parameter Tuning using GridSearchCV

--- ann.py
+++ ann.py
@@ -122,3 +122,33 @@ variance = accuracies.std()
 # Improving the ANN
 # Dropout Regularization to reduce overfitting if needed
 # Lookout for dropout layers below the Dense Layers above.
+
+# Tuning the ANN
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+from keras.models import Sequential
+from keras.layers import Dense
+def build_classifier(optimizer):
+    classifier = Sequential()
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+    classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+    classifier.compile(optimizer = optimizer, loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return classifier
+classifier = KerasClassifier(build_fn = build_classifier)
+parameters = {'batch_size': [25, 32],
+              'epochs': [100, 500],
+              'optimizer': ['adam', 'rmsprop']}
+
+# Always use 'rmsprop' for Recurrent Neural Networks
+
+grid_search = GridSearchCV(estimator = classifier,
+                           param_grid = parameters,
+                           scoring = 'accuracy',
+                           cv = 10)
+grid_search = grid_search.fit(X_train, y_train)
+best_parameters = grid_search.best_params_
+best_accuracy = grid_search.best_score_
+
+# Note : The above process, i.e., tuning the ANN will take quite some time.
+
